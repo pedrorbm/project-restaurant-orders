a65cd4bf1a161ba6ca34b43e11fc2ec835f67a5c
Pedro Medeiros <pedropwks@gmail.com>
2023-11-03 08:57:14 -0300

    Feat: Adicionando testes da classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_main = Ingredient("carne")
+    restriction = {Restriction.ANIMAL_MEAT, Restriction.ANIMAL_DERIVED}
+
+    assert ingredient_main.restrictions == restriction
+
+    assert ingredient_main.name == "carne"
+
+    assert ingredient_main == Ingredient("carne")
+
+    assert hash(ingredient_main) == hash("carne")
+
+    assert repr(ingredient_main) == "Ingredient('carne')"
